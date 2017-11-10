f406547f98c75c67b3127ab9adc9666f23a81f25
Michael Maliniak <M.Maliniak@unswglobal.unsw.edu.au>
2017-11-10 16:47:44 +1100

    added all redirectss

--- private/functions.php
+++ private/functions.php
@@ -17,4 +17,14 @@ function raw_u($string) {
 function h($string) {
     return htmlspecialchars($string);   
 }
+
+function error_404() {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Four oh FOur Not Found");
+    exit();
+}
+
+function error_500() {
+    header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal server error");
+    exit();
+}
 ?>
