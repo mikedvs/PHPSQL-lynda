a332f87ae41546283da0c3379678c65b4c8dc526
mikedvs41 <michael.maliniak@dtdigital.com.au>
2017-11-14 06:55:23 +1100

    reinstated proper subjects page

--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -1,7 +1,4 @@
-
-
-// PHP associative array impersonating an MySQL result!
-
+<?php require_once('../../../private/initialize.php'); ?>
 <?php
     $subjects = [
         ['id'=>'1', 'position'=>'1','visible'=>'1', 'menu_name'=>'About UNSW Global'],
